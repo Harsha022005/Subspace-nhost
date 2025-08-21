8d75512e8afad26ab44f794e8b0cecb7b3ea1c2f
Your Full Name <harshajami2005@gmail.com>
2025-08-21 12:35:23 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,44 +1,38 @@
 import React from 'react';
-import ChatInbox from './Chats/chatinbox';
-import ChatWindow from './Chats/chatroom'; 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Signup from './Auth/Signup';
 import Signin from './Auth/Signin';
+import ChatInbox from './Chats/chatinbox';
+import ChatWindow from './Chats/chatroom';
 import PrivateRoute from './Auth/privateroute';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ApolloProvider } from '@apollo/client';
-import { client } from './appoloclient'; 
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <BrowserRouter>
-        <Routes>
-          {/* Public routes */}
-          <Route path="/" element={<Signin />} />
-          <Route path="/signup" element={<Signup />} />
+    <BrowserRouter>
+      <Routes>
+        {/* Public routes */}
+        <Route path="/" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
 
-          {/* Protected routes */}
-          <Route
-            path="/chatinbox"
-            element={
-              <PrivateRoute>
-                <ChatInbox />
-              </PrivateRoute>
-            }
-          />
-
-          {/* Individual chatroom route */}
-          <Route
-            path="/chatroom/:chatId"
-            element={
-              <PrivateRoute>
-                <ChatWindow />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </ApolloProvider>
+        {/* Protected routes */}
+        <Route
+          path="/chatinbox"
+          element={
+            <PrivateRoute>
+              <ChatInbox />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/chatroom/:chatId"
+          element={
+            <PrivateRoute>
+              <ChatWindow />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,38 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import { ApolloProvider } from '@apollo/client';
+import { client } from './appoloclient';
 import { nhost } from './nhost';
-
-// HTTP Link
-const httpLink = createHttpLink({
-  uri: 'https://mgaffvhkvdrrhibqcqkp.hasura.ap-south-1.nhost.run/v1/graphql',
-});
-
-// Auth Link
-const authLink = setContext(async (_, { headers }) => {
-  const token = await nhost.auth.getAccessToken();
-  return {
-    headers: {
-      ...headers,
-      Authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-// Apollo Client
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+import { NhostReactProvider } from '@nhost/react';
+// Remove NhostApolloProvider - use custom client instead
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <NhostReactProvider nhost={nhost}>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </NhostReactProvider>
   </React.StrictMode>
 );
 
